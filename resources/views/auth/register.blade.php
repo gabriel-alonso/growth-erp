0754db8c094835f71b5c69faab362f92fd39d646
Gabriel Alonso <dev.gabriel.alonso@gmail.com>
2021-01-27 13:37:32 -0300

    add boostrap and page register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,7 @@
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
+            <h2 class="h2">Criar Conta</h2>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -10,22 +11,32 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-label for="first-name" value="{{ __('Primeiro Nome') }}" />
+                <x-jet-input id="first-name" class="block mt-1 w-full" type="text" name="first-name" :value="old('first-name')" required autofocus autocomplete="first-name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="last-name" value="{{ __('Último Nome') }}" />
+                <x-jet-input id="last-name" class="block mt-1 w-full" type="text" name="last-name" :value="old('last-name')" required autofocus autocomplete="last-name" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="email" value="{{ __('E-mail') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="name" value="{{ __('Nome do Usuario') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Senha') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-label for="password_confirmation" value="{{ __('Confirmar senha') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
@@ -48,11 +59,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Já é registrado?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Criar conta') }}
                 </x-jet-button>
             </div>
         </form>
---
+++ sql/sql.sql
@@ -0,0 +1,12 @@
+/*CREATE TABLE USERS(
+	user_id bigint(20) AUTO_INCREMENT NOT NULL, 
+    user_name varchar(60),
+    user_password varchar(255),
+    user_email varchar(100),
+    user_first_name varchar(50),
+    user_last_name varchar(100),
+    user_registered datetime,
+    PRIMARY KEY (user_id)
+);*/
+
+/* SELECT * FROM USERS; */
